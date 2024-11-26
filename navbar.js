897559065ec917875eaf54caab18e5f83a6f3076
TrafishScott <huyncvph48898@gmail.com>
2024-11-27 03:18:08 +0700

    dm

---
+++ navbar.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    var headerHeight = $('.nav-bar').outerHeight();
+    var navbar = document.querySelector('.nav-bar');
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > headerHeight) {
+            setTimeout(() => navbar.classList.add('sticky'), 200);
+        } else {
+            navbar.classList.remove('sticky');
+        }
+    });
+
+    document.getElementById('search-icon').addEventListener('mouseover', function () {
+        const svg = document.getElementById('search-svg').querySelector('path');
+        document.querySelector('.search-input').focus();
+    });
+
+    document.getElementById('search-icon').addEventListener('mouseout', function () {
+        const svg = document.getElementById('search-svg').querySelector('path');
+    });
+
+    $('.nav-item').hover(
+        function () {
+            $('<style>').prop('type', 'text/css').html('.nav-item:hover::after { width: 100%; }').appendTo('head');
+        },
+        function () {
+            $('<style>').prop('type', 'text/css').html('.nav-item::after { width: 0%; }').appendTo('head');
+        }
+    );
+});
--- search.js
+++ search.js
@@ -1,5 +1,15 @@
-function toggleDropdown() {
-    document.querySelector(".search-option").classList.toggle("show");
-}
+$(document).ready(function () {
+    // Khi click vào button dropdown, toggle (hiện/ẩn) menu
+    $('.select-btn').click(function () {
+        $('.select-menu').toggleClass('show');
+    });
 
+    // Đóng dropdown khi click ra ngoài
+    $(document).click(function (event) {
+        if (!$(event.target).closest('.select-menu').length) {
+            $('.select-menu').removeClass('show');
+        }
+    });
 
+    
+});
